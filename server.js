25335607610300a574001efb10dacec8e8407e33
Alexey Stulikov <o1h@ya.ru>
2017-11-03 18:27:28 +0400

    releasing v1

--- server.js
+++ server.js
@@ -2,21 +2,78 @@ var http = require("http");
 var url = require("url");
 var fs = require("fs");
 var qs = require('querystring');
+var SerialPort = require('serialport');
+
 const requestIp = require('request-ip');
-const ethalon_token = "";
+const ethalon_token = process.env.ETHALON_TOKEN || "TgYgtt822NeQaqLQ9XQWtFhI";
+const ethalon_team_id = process.env.ETHALON_TEAM_ID || "T7J3M65J4";
+const debug = true;
 
 var port = process.env.PORT || 8081;
 var logfile = "./log.txt";
 
-function process_request(data) {
+var inputString = "";
+var sport = new SerialPort('/dev/tty.SLAB_USBtoUART', {
+	baudRate: 9600
+});
 
+sport.on('open', function() {
+	console.log('Serial port opened.');
+}).on('error', function(err) {
+	console.log('Error: ', err.message);
+});
+
+sport.on('data', (data) => {
+	if(data.slice(-1,1) == "\n") {
+		console.log(inputString);
+		inputString = "";
+	} else {
+		inputString += data.toString();
+	}
+});
+
+function process_request(data) {
+	if (data.token == ethalon_token && data.team_id == ethalon_team_id) {
+		log_opener(data.user_id, data.command, "authiorized");
+		if (data.command == "/door_cafe" || data.command == "/t") {
+			open_door("door_cafe");
+			return [true, "Opening CAFE side door"];
+		} else if (data.command == "/door_smoke" || data.command == "/y") {
+			open_door("door_smoke");
+			return [true, "Opening SMOKE exit side door"];
+		} else if (data.command == "/door_sortir" || data.command == "/u") {
+			open_door("door_sortir");
+			return [true, "Opening SORTIR side door"];
+		// } else if (data.command == "/n") {
+		// 	sport.close(function (err) { console.log("serial port closed " + err) });
+		// 	return [true, "Serial port closed"];
+		} else {
+			if (debug) { console.log("wrong command"); }
+			log_opener(data.user_id, data.command, "wrong command");
+			return [false, "wrong command"];
+		}
+	} else {
+		if (debug) { console.log("not authorized"); }
+		log_opener(data.user_id, data.command, "not authorized");
+		return [false, "not authorized"];
+	}
 }
+
+function open_door(door) {
+	sport.write(door + "\n", function(err) {
+		if (err) {
+			return console.log('Error on write: ', err.message);
+		}
+		console.log('Asking to open the ' + door);
+	});
+}
+
+function log_opener(user, command, auth) {}
 
 http.createServer(function (request, response) {
 	// Send the HTTP header 
 	// HTTP Status: 200 : OK
 	// Content Type: text/plain
-	// response.writeHead(200, {'Content-Type': 'application/x-www-form-urlencoded'});
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	
 
@@ -28,9 +85,17 @@ http.createServer(function (request, response) {
 			body += data;
 		});
 		request.on('end',function(){
-			var obj = JSON.parse(body);
-			console.log(obj);
+			var obj = qs.parse(body);
+			if (debug) { console.log(obj); }
+			var processing_result = process_request(obj);
+			if (processing_result[0]) {
+				response.end(processing_result[1]);
+			} else {
+				response.end(":pashak:");
+			}
 		});
+	} else {
+		response.end("nothing");
 	}
 
 	var IP = requestIp.getClientIp(request);
@@ -39,5 +104,5 @@ http.createServer(function (request, response) {
 }).listen(port);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:' + port + '/');
+console.log('Server running at port ' + port);
 
