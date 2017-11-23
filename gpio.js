e7da4b52967f3b0db2419d4bbf8e8a0bf19c95b0
Krutov Alex <goozler@mail.ru>
2017-11-23 15:20:45 +0400

    Make eslint a little happier

--- gpio.js
+++ gpio.js
@@ -7,48 +7,52 @@ const db = require('node-localdb');
 
 const debug = true;
 
-const ethalon_token = process.env.BACKDOOR_ETHALON_TOKEN;
-const ethalon_team_id = process.env.BACKDOOR_ETHALON_TEAM_ID;
+const ethalonToken = process.env.BACKDOOR_ETHALON_TOKEN;
+const ethalonTeamID = process.env.BACKDOOR_ETHALON_TEAM_ID;
 const port = process.env.BACKDOOR_PORT || 8081;
 
 // CAFE, SMOKE, SORTIR
 const gpios = [11, 13, 15];
-const gpios_ready_states = [false, false, false];
+const gpiosReadyStates = [false, false, false];
 
 const logfile = '/home/pi/doorway/log.txt';
 const users = db('/home/pi/doorway/users.json');
-const inputString = '';
 
-function init_door(i) {
+function initDoor(i) {
   gpio.setup(gpios[i], gpio.DIR_OUT, gpio.EDGE_NONE, () => {
-    gpios_ready_states[i] = true;
-    console.log('GPIO ready state', gpios_ready_states[i]);
+    gpiosReadyStates[i] = true;
+    console.log('GPIO ready state', gpiosReadyStates[i]);
   });
 }
 
-function gpio_init() {
+function gpioInit() {
   gpio.destroy(() => {
     console.log('All pins unexported.');
-    for (let i = 0; i < gpios.length; i++) {
-      gpios_ready_states[i] = false;
-      init_door(i);
+    for (let i = 0; i < gpios.length; i += 1) {
+      gpiosReadyStates[i] = false;
+      initDoor(i);
     }
   });
 }
-gpio_init();
+gpioInit();
 
-function add_user(user_id, user_name, callback) {
-  users.findOne({ userid: user_id }).then((u) => {
+function addUser(userID, userName, callback) {
+  users.findOne({ userid: userID }).then((u) => {
     if (u) {
-      let access_txt = '';
-      if (u.access) { access_txt = ', access allowed'; } else { access_txt = ', access denied, ask boss to get an access.'; }
+      let accessTxt = '';
+      if (u.access) { accessTxt = ', access allowed'; } else { accessTxt = ', access denied, ask boss to get an access.'; }
 
-      let admin_txt = '';
-      if (u.admin) { admin_txt = ', admin'; }
+      let adminTxt = '';
+      if (u.admin) { adminTxt = ', admin'; }
 
-      callback([false, `User already exist${access_txt}${admin_txt}.`]);
+      callback([false, `User already exist${accessTxt}${adminTxt}.`]);
     } else {
-      users.insert({ userid: user_id, username: user_name, access: false, admin: false }).then((au) => {
+      users.insert({
+        userid: userID,
+        username: userName,
+        access: false,
+        admin: false,
+      }).then((au) => {
         console.log(`Added user: ${au}`);
         callback([true, `You added as ${au.username}`]);
       });
@@ -56,9 +60,9 @@ function add_user(user_id, user_name, callback) {
   });
 }
 
-function access_allowed(data, db_callback) {
+function accessAllowed(data, dbCallback) {
   let result = 0;
-  if (data.token == ethalon_token && data.team_id == ethalon_team_id) {
+  if (data.token === ethalonToken && data.team_id === ethalonTeamID) {
     result = 1;
   }
   users.findOne({ userid: data.user_id }).then((u) => {
@@ -71,30 +75,30 @@ function access_allowed(data, db_callback) {
       }
     }
     console.log(result);
-    db_callback(result);
+    dbCallback(result);
   });
 }
 
-function list_users(db_callback) {
+function listUsers(dbCallback) {
   users.find({}).then((us) => {
     let result = '';
-    for (let i = 0; i < us.length; i++) {
-      const access_txt = us[i].access ? ' allowed' : ' disabled';
-      const admin_txt = us[i].admin ? ' admin' : '';
-      result += `${us[i].userid}: ${us[i].username}${access_txt}${admin_txt}\n`;
+    for (let i = 0; i < us.length; i += 1) {
+      const accessTxt = us[i].access ? ' allowed' : ' disabled';
+      const adminTxt = us[i].admin ? ' admin' : '';
+      result += `${us[i].userid}: ${us[i].username}${accessTxt}${adminTxt}\n`;
     }
     console.log(result);
-    db_callback([true, result]);
+    dbCallback([true, result]);
   });
 }
 
-function user_permissions(params, db_callback) {
-  user_id = params.user_id;
-  users.findOne({ userid: user_id }).then((u) => {
+function userPermissions(params, dbCallback) {
+  const userID = params.user_id;
+  users.findOne({ userid: userID }).then((u) => {
     if (u) {
-      let user_name = u.username;
+      let userName = u.username;
       if (typeof (params.user_name) !== 'undefined') {
-        user_name = params.user_name;
+        userName = params.user_name;
       }
       let access = u.access;
       if (typeof (params.access) !== 'undefined') {
@@ -105,113 +109,116 @@ function user_permissions(params, db_callback) {
         admin = params.admin;
       }
 
-      users.remove({ userid: user_id }).then((ru) => {
-        users.insert({ userid: user_id, username: user_name, access, admin }).then((au) => {
+      users.remove({ userid: userID }).then(() => {
+        users.insert({ userid: userID, username: userName, access, admin }).then((au) => {
           console.log('User was changed: ', au);
-          let access_txt = '';
-          if (access != u.access) {
-            access_txt = access ? ' now allowed to open doors' : ' now prohibited to open doors';
+          let accessTxt = '';
+          if (access !== u.access) {
+            accessTxt = access ? ' now allowed to open doors' : ' now prohibited to open doors';
           }
-          let admin_txt = '';
-          if (admin != u.admin) {
-            admin_txt = admin ? ' now admin' : ' now not admin';
+          let adminTxt = '';
+          if (admin !== u.admin) {
+            adminTxt = admin ? ' now admin' : ' now not admin';
           }
-          if (access_txt == '' && admin_txt == '') {
-            admin_txt = ': nothing to change. Everything is up to date';
+          if (accessTxt === '' && adminTxt === '') {
+            adminTxt = ': nothing to change. Everything is up to date';
           }
-          db_callback([true, `${user_name + access_txt + admin_txt}.`]);
+          dbCallback([true, `${userName + accessTxt + adminTxt}.`]);
         });
       });
     } else {
-      db_callback([true, "Such user doesn't exist."]);
+      dbCallback([true, "Such user doesn't exist."]);
     }
   });
 }
 
-function process_request(data, IP, request, callback) {
-  access_allowed(data, (result) => {
+function processRequest(data, IP, request, callback) {
+  accessAllowed(data, (result) => {
     if (result >= 2) {
-      log_opener(`IP: ${IP} user_id: ${data.user_id}; parsed command: ${data.command}; auth result: authorized; request.url: ${request.url}`);
-      if (data.command == '/door_cafe' || data.command == '/t') {
-        open_door(0);
+      logOpener(`IP: ${IP} user_id: ${data.user_id}; parsed command: ${data.command}; ` +
+                `auth result: authorized; request.url: ${request.url}`);
+      if (data.command === '/door_cafe' || data.command === '/t') {
+        openDoor(0);
         callback([true, 'Opening CAFE side door']);
-      } else if (data.command == '/door_smoke' || data.command == '/y') {
-        open_door(1);
+      } else if (data.command === '/door_smoke' || data.command === '/y') {
+        openDoor(1);
         callback([true, 'Opening SMOKE side door']);
-      } else if (data.command == '/door_sortir' || data.command == '/u') {
-        open_door(2);
+      } else if (data.command === '/door_sortir' || data.command === '/u') {
+        openDoor(2);
         callback([true, 'Opening SORTIR side door']);
-      } else if (data.command == '/door_update_my_name') {
-        user_permissions({ user_id: data.user_id, user_name: data.user_name }, callback);
-      } else if (data.command == '/da_list_users') {
-        if (result == 3) {
-          list_users(callback);
+      } else if (data.command === '/door_update_my_name') {
+        userPermissions({ userID: data.user_id, userName: data.user_name }, callback);
+      } else if (data.command === '/da_list_users') {
+        if (result === 3) {
+          listUsers(callback);
         } else {
           callback([false, 'You are not admin, access denied.']);
         }
-      } else if (data.command == '/da_grant_permissions') {
-        if (result == 3) {
-          user_permissions({ user_id: data.text, access: true }, callback);
+      } else if (data.command === '/da_grant_permissions') {
+        if (result === 3) {
+          userPermissions({ userID: data.text, access: true }, callback);
         } else {
           callback([false, 'You are not admin, access denied.']);
         }
-      } else if (data.command == '/da_revoke_permissions') {
-        if (result == 3) {
-        user_permissions({ user_id: data.text, access: false }, callback);
+      } else if (data.command === '/da_revoke_permissions') {
+        if (result === 3) {
+          userPermissions({ userID: data.text, access: false }, callback);
+        } else {
+          callback([false, 'You are not admin, access denied.']);
+        }
+      } else if (data.command === '/da_make_admin') {
+        if (result === 3) {
+          userPermissions({ userID: data.text, admin: true }, callback);
+        } else {
+          callback([false, 'You are not admin, access denied.']);
+        }
+      } else if (data.command === '/da_disrank_admin') {
+        if (result === 3) {
+          userPermissions({ userID: data.text, admin: false }, callback);
+        } else {
+          callback([false, 'You are not admin, access denied.']);
+        }
       } else {
-        callback([false, 'You are not admin, access denied.']);
+        if (debug) { console.log('wrong command'); }
+        logOpener(`IP: ${IP} user_id: ${data.user_id}; parsed command: ${data.command}; ` +
+                  `auth result: authorized, wrong command; request.url: ${request.url}`);
+        callback([false, 'wrong command']);
       }
-      } else if (data.command == '/da_make_admin') {
-      if (result == 3) {
-      user_permissions({ user_id: data.text, admin: true }, callback);
-    } else {
-      callback([false, 'You are not admin, access denied.']);
-    }
-    } else if (data.command == '/da_disrank_admin') {
-    if (result == 3) {
-    user_permissions({ user_id: data.text, admin: false }, callback);
-  } else {
-    callback([false, 'You are not admin, access denied.']);
-  }
-  } else {
-    if (debug) { console.log('wrong command'); }
-    log_opener(`IP: ${IP} user_id: ${data.user_id}; parsed command: ${data.command}; auth result: authorized, wrong command; request.url: ${request.url}`);
-    callback([false, 'wrong command']);
-  }
-    } else if (result == 1 && data.command == '/door_give_me_access') {
-      add_user(data.user_id, data.user_name, callback);
+    } else if (result === 1 && data.command === '/door_give_me_access') {
+      addUser(data.user_id, data.user_name, callback);
     } else {
       if (debug) { console.log('not authorized'); }
-      log_opener(data.user_id, data.command, 'not authorized', IP);
+      logOpener(data.user_id, data.command, 'not authorized', IP);
       callback([false, 'not authorized']);
     }
   });
   console.log('', data);
 }
 
-function open_door(door_id) {
-  console.log('open door ', gpios_ready_states, door_id);
-  if (gpios_ready_states[door_id]) {
-    gpio.write(gpios[door_id], 1, (err) => {
+function openDoor(doorID) {
+  console.log('open door ', gpiosReadyStates, doorID);
+  if (gpiosReadyStates[doorID]) {
+    gpio.write(gpios[doorID], 1, (err) => {
       if (err) {
         console.log('Error on GPIO write 1: ', err.message);
       }
-      console.log(`Asking to open door #${door_id}`);
+      console.log(`Asking to open door #${doorID}`);
 
       setTimeout(() => {
-        gpio.write(gpios[door_id], 0, (err) => {
-          if (err) {
+        gpio.write(gpios[doorID], 0, (e) => {
+          if (e) {
             console.log('Error on GPIO write 0: ', err.message);
           }
         });
       }, 200);
     });
   } else {
-    console.log(`GPIO port #${gpios[door_id]} isn't ready to operate.`);
+    console.log(`GPIO port #${gpios[doorID]} isn't ready to operate.`);
   }
 }
 
-function log_opener(line) {
+
+function logOpener(line) {
   const time = (new Date()).toLocaleTimeString();
   fs.appendFile(logfile, `${time} ${line}\n`, (err) => {
     if (err) throw err;
@@ -224,7 +231,7 @@ function log_opener(line) {
 http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  if (request.method == 'POST') {
+  if (request.method === 'POST') {
     let body = '';
     request.on('data', (data) => {
       body += data;
@@ -233,7 +240,7 @@ http.createServer((request, response) => {
       const obj = qs.parse(body);
       if (debug) { console.log(obj); }
       const IP = requestIp.getClientIp(request);
-      process_request(obj, IP, request, (result) => {
+      processRequest(obj, IP, request, (result) => {
         if (result[0]) {
           response.end(result[1]);
         } else {
@@ -246,5 +253,5 @@ http.createServer((request, response) => {
   }
 }).listen(port, '127.0.0.1');
 
-console.log(`Server running at port ${port}; Token: ${ethalon_token} Team ID: ${ethalon_team_id}.`);
-log_opener(`Server running at port ${port}; Token: ${ethalon_token} Team ID: ${ethalon_team_id}.`);
+console.log(`Server running at port ${port}; Token: ${ethalonToken} Team ID: ${ethalonTeamID}.`);
+logOpener(`Server running at port ${port}; Token: ${ethalonToken} Team ID: ${ethalonTeamID}.`);
